0ddc84be2f1d0bde7188b8ca8c2808ffd2dd02b5
梁志煜 <liangzhiyu@coding.net>
2019-10-31 10:39:05 +0800

    add UserController

---
+++ src/main/java/com/liam/oauth2/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.liam.oauth2.controller;
+
+import com.liam.oauth2.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping("/t-save")
+    public String save(){
+        userService.save();
+        return "success";
+    }
+
+}
