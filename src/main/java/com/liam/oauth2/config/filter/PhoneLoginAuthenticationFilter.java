144e428075e045fb6da97c7e7bc278fc65c60a17
superwaltz <lzyyzllzy@163.com>
2021-01-08 15:18:08 +0800

    remove unnecessary reference

--- src/main/java/com/liam/oauth2/config/filter/PhoneLoginAuthenticationFilter.java
+++ src/main/java/com/liam/oauth2/config/filter/PhoneLoginAuthenticationFilter.java
@@ -1,11 +1,8 @@
 package com.liam.oauth2.config.filter;
 
-import org.springframework.security.authentication.AuthenticationServiceException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
